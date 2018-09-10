de52b46b01d8fc33321b6856b2c424c4c3d989ef
dubininv <v2.dubinin@gmail.com>
2018-09-10 15:33:53 +0300

    Движение змейки

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -59,5 +59,11 @@ namespace Snake
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
+
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
     }
 }
--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -8,8 +8,11 @@ namespace Snake
 {
     class Snake : Figure
     {
+        Direction direction;
+
         public Snake(Point tail, int length, Direction direction)
         {
+            this.direction = direction;
             pList = new List<Point>();
             for( int i = 0; i < length; i++)
             {
@@ -17,6 +20,25 @@ namespace Snake
                 p.Move(i, direction);
                 pList.Add(p);
             }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
         }
     }
 }
